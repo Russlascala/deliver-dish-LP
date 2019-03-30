9c16a8e7490ba5137e84b292cb1cbad38d0a67db
Russ Lascala <russlascala@gmail.com>
2019-03-30 12:52:44 -0400

    made final styles

--- js/script.js
+++ js/script.js
@@ -1,15 +1,4 @@
-/* Top Month Script */
-/* Month Array */
-const monthNames = ["January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"
-];
-/* Gets month and year */
-const d = new Date();
-var month = monthNames[d.getMonth()];
-console.log([d.getMonth()]);
-var year = d.getFullYear();
-/* inputs the month and year value on top of page  */
-document.getElementById("dateTop").innerHTML = (month + " " + year);
+
 
 
 /* Script to change the column stacking for mobile to get list on bottom of form */
